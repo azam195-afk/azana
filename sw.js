6e71af139ad44a1f1e1261a85a52d9afc8fbdf05
azam195-afk <maulanaazam195@gmail.com>
2026-05-08 01:58:35 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,58 +1,25 @@
-const CACHE_NAME = 'azana-cache-v8'; // Gue naikin ke v8 biar fresh
-
-// Daftar semua file (Tanpa Tailwind dan Folder sudah 'asset')
-const urlsToCache = [
+const CACHE_NAME = 'azana-v11'; // Ganti v11 biar bener-bener reset
+const assets = [
   './',
   'index.html',
-  'blogs.html',
+  'manifest.json',
   'offline.html',
-  'penjernih.html',
-  'eraser.html',
-  'removebg.html',
-  'privacy-policy.html',
-  'terms.html',
-  'about.html',
-  'artikel1.html',
-  'artikel2.html',
-  'artikel3.html',
-  'components/navbar.html',
-  'components/footer-main.html',
-  'asset/img/41955.png', 
-  'asset/img/globe.png',
-  'manifest.json'
+  'asset/img/41955.png',
+  'asset/img/globe.png'
 ];
 
-// Tahap Install
-self.addEventListener('install', event => {
-  event.waitUntil(
+self.addEventListener('install', e => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('Sistem Azana: Sedang mendownload aset...');
-      return cache.addAll(urlsToCache);
+      return cache.addAll(assets);
     })
   );
 });
 
-// Tahap Fetch
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
-});
-
-// Tahap Activate
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Menghapus cache lama...');
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
     })
   );
 });
