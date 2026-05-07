f8b7bbef059be09c0ab95a1e5a68335f55a35588
azam195-afk <maulanaazam195@gmail.com>
2026-05-08 01:06:21 +0700

    Update service worker to use new cache version and assets

--- sw.js
+++ sw.js
@@ -1,56 +1,26 @@
-const CACHE_NAME = 'azana-cache-v1';
-
-// Daftar semua file yang akan di-download otomatis saat pertama buka web
-const urlsToCache = [
+const CACHE_NAME = 'azana-v2'; // Ganti v1 ke v2 biar browser anggap ini file baru
+const assets = [
   './',
   'index.html',
   'manifest.json',
   'offline.html',
   'components/navbar.html',
   'components/footer-main.html',
-  'assets/img/globe.png', // Ikon utama aja
-  'https://cdn.tailwindcss.com'
+  'assets/img/globe.png'
 ];
 
-
-// Tahap Install: Download semua file di atas ke memori HP
-self.addEventListener('install', event => {
-  event.waitUntil(
+self.addEventListener('install', e => {
+  e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('Sistem Azana: Sedang mendownload aset...');
-      return cache.addAll(urlsToCache);
+      return cache.addAll(assets);
     })
   );
 });
 
-// Tahap Fetch: Ambil data dari memori kalau internet mati
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      const fetchPromise = fetch(event.request).then(networkResponse => {
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, networkResponse.clone());
-        });
-        return networkResponse;
-      });
-      return cachedResponse || fetchPromise;
-    })
-  );
-});
-
-
-// Tahap Activate: Bersihkan cache lama kalau ada versi baru
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Menghapus cache lama...');
-            return caches.delete(cacheName);
-          }
-        })
-      );
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
     })
   );
 });
